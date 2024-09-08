910a4ffd41be7f05bf55eccb01e8edac03d7a691
Bread <168324037+Breadisfunie@users.noreply.github.com>
2024-09-08 14:07:27 -0300

    Bruh

---
+++ public/js/Report-bot.js
@@ -0,0 +1,27 @@
+let reportUsername;
+let reason;
+
+document.getElementById("reportButton").onclick = async function(){ 
+    //Sends the report
+    reportUsername = document.getElementById("reportUsername").value;
+    reason = document.getElementById("reason").value;
+    //Checks if its blank
+    if (reportUsername == "" || reason == "")  {
+        console.log("invalid user")
+    } else {
+        console.log("Username is " + reportUsername + " reason is " + reason);
+        //Tells the user that the report went though
+        document.getElementById("reportButton").textContent = "User reported"
+        //Deletes the text afterwards
+        document.getElementById("reportUsername").value = "";
+        document.getElementById("reason").value = "";
+        //Sets the report button back lol
+        await sleep(5);
+        document.getElementById("reportButton").textContent = "Report"
+    }
+}
+
+//meth
+async function sleep(seconds) {
+    return new Promise((resolve) =>  setTimeout(resolve, seconds * 1000));
+  }

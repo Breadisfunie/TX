76943def4df532c6a1721f1208a32da488896cfb
Bread <168324037+Breadisfunie@users.noreply.github.com>
2024-09-13 08:34:03 -0300

    cookie

--- js/Report-bot.js
+++ js/Report-bot.js
@@ -1,5 +1,6 @@
 let reportUsername;
 let reason;
+let termcount = document.cookie(0);
 
 document.getElementById("reportButton").onclick = async function(){ 
     //Sends the report
@@ -12,6 +13,7 @@ document.getElementById("reportButton").onclick = async function(){
         console.log("Username is " + reportUsername + " reason is " + reason);
         //Tells the user that the report went though
         document.getElementById("reportButton").textContent = "User reported"
+        document.cookie + 1;
         //Deletes the text afterwards
         document.getElementById("reportUsername").value = "";
         document.getElementById("reason").value = "";

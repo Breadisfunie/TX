77cd616304d9b3911a37fc74f2624097ce237e22
Bread <168324037+Breadisfunie@users.noreply.github.com>
2024-09-22 14:53:06 -0300

    Big ass update

--- js/Report-bot.js
+++ js/Report-bot.js
@@ -1,6 +1,11 @@
 let reportUsername;
 let reason;
-var termcount = 0;
+var termcount = document.cookie;
+if (termcount == 0) {
+    document.getElementById("termcountdisplay").textContent = 0;
+} else {
+    document.getElementById("termcountdisplay").textContent = termcount;
+}
 
 document.getElementById("reportButton").onclick = async function(){ 
     //Sends the report
@@ -13,12 +18,14 @@ document.getElementById("reportButton").onclick = async function(){
         console.log("Username is " + reportUsername + " reason is " + reason);
         //Tells the user that the report went though
         document.getElementById("reportButton").textContent = "User reported"
-        termcount += 1;
+        termcount++; //adds to the term count
         console.log("termcount is " + termcount)
         //Deletes the text afterwards
         document.getElementById("reportUsername").value = "";
         document.getElementById("reason").value = "";
         //Sets the report button back lol
+        document.cookie = termcount;
+        document.getElementById("termcountdisplay").textContent = termcount;
         await sleep(5);
         document.getElementById("reportButton").textContent = "Report"
     }

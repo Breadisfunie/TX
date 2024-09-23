ffe039677c42c0ff9524e5bfc7aafc1a1e92157b
Bread <168324037+Breadisfunie@users.noreply.github.com>
2024-09-23 20:10:24 -0300

    yes

--- js/darkModeButtonAbout.js
+++ js/darkModeButtonAbout.js
@@ -24,6 +24,7 @@ document.getElementById("darkModeButton").onclick = async function(){
          document.getElementById("suporter-usernames-2").style.color = "black";
          document.getElementById("suporter-usernames-3").style.color = "black";
          document.getElementById("suporter-usernames-4").style.color = "black";
+         document.getElementById("suporter-usernames-5").style.color = "black";
          darkmodeactive = false; //makes it false lol  
         }  else { // runs if light mode is active// why do i make these comments when no one reads them send me a dm if u read this lol
          //Runs if light mode it active // makes it dark again
@@ -46,6 +47,7 @@ document.getElementById("darkModeButton").onclick = async function(){
          document.getElementById("suporter-usernames-2").style.color = "white";
          document.getElementById("suporter-usernames-3").style.color = "white";
          document.getElementById("suporter-usernames-4").style.color = "white";
+         document.getElementById("suporter-usernames-5").style.color = "white";
          darkmodeactive = true;
     }
 }
